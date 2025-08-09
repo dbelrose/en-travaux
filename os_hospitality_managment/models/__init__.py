b3cfcecfbf7ab0b1073f6ade8abf3c2456d59532
Didier Belrose <didier.belrose@gmail.com>
2025-08-08 17:18:36 -1000

    [ADD] Expose code to AI

--- os_hospitality_managment/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import booking_import
-from . import booking_import_line
-from . import booking_import_wizard
-from . import booking_manual_wizard
--- os_hospitality_managment/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    incoterm_id = fields.Many2one('account.incoterms', string='Default incoterm',
-                                  help='International Commercial Terms are a series of predefined commercial terms '
-                                       'used in international transactions.')
