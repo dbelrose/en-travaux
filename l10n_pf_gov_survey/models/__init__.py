aaa3e30d30dad6097012be3f34165211e750f810
Didier Belrose <didier.belrose@gmail.com>
2026-03-13 10:38:21 -1000

    [UPD] Public code

---
+++ l10n_pf_gov_survey/models/__init__.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import survey
+from . import survey_question
+from . import survey_question_answer
+from . import survey_user_input
+from . import survey_user_input_line
