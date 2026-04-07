c203a06226bc7204918c2cb1cde77484023a201c
Didier Belrose <didier.belrose@gmail.com>
2026-04-06 19:21:03 -1000

    [UPD] Add and remove exposed modules

---
+++ os_auxiliaire_medical/__manifest__.py
@@ -0,0 +1,37 @@
+{
+    'name': 'OS Auxiliaire Médical',
+    'version': '17.0.1.0.0',
+    'summary': 'Gestion des feuilles de soins et bordereaux CPS Polynésie française',
+    'description': """
+Gestion complète des feuilles de soins auxiliaires médicaux pour la
+Caisse de Prévoyance Sociale (CPS) de Polynésie française.
+
+Fonctionnalités :
+- Saisie des feuilles de soins (FSA25)
+- Gestion des patients / bénéficiaires
+- Génération automatique des bordereaux mensuels
+- Export Excel et PDF des bordereaux
+- Suivi tiers payant (CPS + patient)
+""",
+    'author': 'OpalSea',
+    'website': 'https://www.opalsea.site',
+    'category': 'Healthcare',
+    'license': 'LGPL-3',
+    'depends': ['base', 'mail', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'views/praticien_views.xml',
+        'views/patient_views.xml',
+        'views/feuille_soins_views.xml',
+        'views/bordereau_views.xml',
+        'views/menu_views.xml',
+        'report/report_bordereau.xml',
+        'report/report_feuille_soins.xml',
+        'wizards/wizard_bordereau_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'icon': 'static/description/icon.png',
+}
---
+++ os_auxiliaire_medical/models/__init__.py
@@ -0,0 +1,4 @@
+from . import praticien
+from . import patient
+from . import feuille_soins
+from . import bordereau
